1b796e92ba5840a97f1eeba27db60e4489ad18ed
Håkan Gudmundsson <hakgu806@student.liu.se>
2017-02-15 12:55:16 +0100

    First commit

---
+++ twidder/server.py
@@ -0,0 +1,140 @@
+import database_helper as dh
+from random import randint
+from flask import request, Flask
+import json
+
+app = Flask(__name__)
+
+logged_in_users = {}
+
+@app.route('/signin', methods=['POST'])
+def sign_in():
+    email = request.form['email']
+    password = request.form['password']
+    if dh.validate_password(email, password):
+        letters = "abcdefghiklmnopqrstuvwwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        token = "";
+        for i in range(36):
+          token += letters[randint(0, len(letters)-1)]
+          
+        logged_in_users[token] = email
+        return json.dumps({"success": True, "message": "You are now signed in", "data": token})        
+    else:
+        return json.dumps({"success": False, "message": "Invalid email or password"})
+
+@app.route('/signup', methods=['POST'])
+def sign_up():
+    email = request.form['email']
+    password = request.form['password']
+    firstname = request.form['firstname']
+    familyname = request.form['familyname']
+    gender = request.form['gender']
+    city = request.form['city']
+    country = request.form['country']
+    if dh.validate_user(email) == False:
+        dh.sign_up(email, password, firstname, familyname, gender, city, country)
+        return json.dumps({"success": True, "message": "Successfully created a new user"}) 
+
+    return json.dumps({"success": False, "message": "User already exists"})
+    
+@app.route('/signout', methods=['POST'])
+def sign_out():
+    token = request.form['token']
+    if token in logged_in_users:
+        del logged_in_users[token]
+        sign_out(token)
+        return json.dumps({"success": True, "message": "Successfully signed out"})
+    
+    return json.dumps({"success": False, "message": "You are not signed in"})
+
+@app.route('/changepassword', methods=['POST'])
+def change_password():
+    token = request.form['token']
+    old_password = request.form['old_password']
+    new_password = request.form['new_password']
+    if token in logged_in_users:
+        email = logged_in_users[token]
+        if dh.validate_password(email, old_password):
+            dh.update_password(email, new_password)
+            return json.dumps({"success": True, "message": "Password changed"})
+        else:
+            return json.dumps({"success": False, "message": "Wrong password"})
+
+    return json.dumps({"success": False, "message": "You are not signed in"})
+
+@app.route('/getuserdatabytoken', methods=['POST'])
+def get_user_data_by_token():
+    token = request.form['token']
+    if token in logged_in_users:
+        email = logged_in_users[token]
+        return get_user_data(email)
+
+    return json.dumps({"success": False, "message": "You are not signed in"})
+
+@app.route('/getuserdatabyemail', methods=['POST'])
+def get_user_data_by_email():
+    token = request.form['token']
+    if token in logged_in_users:
+        email = request.form['email']
+        return get_user_data(email)
+
+    return json.dumps({"success": False, "message": "You are not signed in"})
+
+def get_user_data(email):
+    data = dh.get_user_data(email)
+    if data:
+        user = {
+            'email': data[0],
+            'firstname': data[2],
+            'familyname': data[3],
+            'gender': data[4],
+            'city': data[5],
+            'country': data[6]
+        }
+        return json.dumps({"success": True, "message": "User data retrieved", "data": user}) 
+
+    return json.dumps({"success": False, "message": "No such user"})
+
+@app.route('/getusermessagesbytoken', methods=['POST'])
+def get_user_messages_by_token():
+    token = request.form['token']
+    if token in logged_in_users:
+        email = logged_in_users[token]
+        return get_user_messages(email)
+        
+    return json.dumps({"success": False, "message": "You are not signed in"})
+
+@app.route('/getusermessagesbyemail', methods=['POST'])
+def get_user_messages_by_email():
+    token = request.form['token']
+    if token in logged_in_users:
+        email = request.form['email']
+        return get_user_messages(email)
+
+    return json.dumps({"success": False, "message": "You are not signed in"})
+
+def get_user_messages(email):
+    messages = dh.get_user_messages(email)
+    if messages:
+        return json.dumps({"success": True, "message": "User messages retrieved", "data": messages})
+
+    return json.dumps({"success": False, "message": "No such user"})
+
+@app.route('/postmessage', methods=['POST'])
+def post_message():
+    token = request.form['token']
+    message = request.form['message']
+    reciever_email = request.form['email']
+    if token in logged_in_users:
+        sender_email = logged_in_users[token]
+        if dh.validate_user(reciever_email):
+            dh.post_message(reciever_email, sender_email, message)
+            return json.dumps({"success": True, "message": "Message posted"})
+        else:
+            return json.dumps({"success": False, "message": "No such user"})
+
+    return json.dumps({"success": False, "message": "You are not signed in"})
+    
+if __name__ == "__main__":
+    dh.init_db()
+    app.run()
---
+++ twidder/static/client.js
@@ -0,0 +1,246 @@
+displayView = function () {
+	var token = localStorage.getItem("token");
+	var view;
+	if (token == null) {
+		view = document.getElementById("welcomeview").innerHTML;
+	}
+	else {
+		view = document.getElementById("profileview").innerHTML;
+	}
+
+    document.getElementById("maincontainer").innerHTML = view;
+    attachHandlers();
+};
+
+attachHandlers = function () {
+    var welcome = document.getElementById("welcome");
+    if (welcome != null) {
+		var signUpForm = document.getElementById("signup");
+		var loginForm = document.getElementById("login");
+        var loginButton = document.getElementById("loginbutton");
+		var signUpButton = document.getElementById("signupbutton");
+        loginButton.addEventListener("click", function () {
+			loginForm.setAttribute("onsubmit", "submitLoginForm();return false;");
+        });
+		
+        signUpButton.addEventListener("click", function () {
+            signUpForm.setAttribute("onsubmit", "submitSignUpForm();return false;");
+		});
+    }
+
+    var profile = document.getElementById("profile");
+    if (profile != null) {
+        var changePwordButton = document.getElementById("changepwordbutton");
+        var changePwordForm = document.getElementById("changepwordform");
+        changePwordButton.addEventListener("click", function () {
+            changePwordForm.setAttribute("onsubmit", "changePassword(this);return false");
+        });
+
+        document.getElementById("signoutbutton").onclick = function () {
+            var token = localStorage.getItem("token");
+            localStorage.removeItem("token");
+            serverstub.signOut(token);
+            displayView();
+        };
+
+        document.getElementById("search").onclick = function () {
+            var email = document.getElementById("accountsearch").value;
+            searchForUser(email);
+        };
+
+        document.getElementById("homebutton").onclick = function () {
+            displayHome();
+        };
+
+        document.getElementById("browsebutton").onclick = function () {
+            clearMessages();
+            displayBrowse();
+            document.getElementById("account").style.display = "none";
+            document.getElementById("home").style.display = "none";
+            document.getElementById("browse").style.display = "block";
+        };
+
+        document.getElementById("accountbutton").onclick = function () {
+            clearMessages();
+            document.getElementById("account").style.display = "block";
+            document.getElementById("home").style.display = "none";
+            document.getElementById("browse").style.display = "none";
+        };
+
+        displayHome();
+    }
+};
+
+displayHome = function (email) {
+    clearMessages();
+    loadWall(email);
+    fillInUserInfo(email);
+    document.getElementById("account").style.display = "none";
+    document.getElementById("home").style.display = "block";
+    document.getElementById("browse").style.display = "none";
+    document.getElementById("reload").onclick = function () {
+            loadWall(email);
+        };
+
+    document.getElementById("post").onclick = function () {
+            var message = document.getElementById("postmessage").value;
+            var token = localStorage.getItem("token");
+            var postEmail;
+            if (email == null) {
+                postEmail = serverstub.getUserDataByToken(token).email;
+            }
+            else {
+                postEmail = email;
+            }
+            serverstub.postMessage(token, message, postEmail);
+            document.getElementById("postmessage").value = "";
+        };
+};
+
+displayBrowse = function () {
+    document.getElementById("browse").innerHTML;
+};
+
+clearMessages = function () {
+    document.getElementById("successmessage").innerHTML = "";
+    document.getElementById("errormessage").innerHTML = "";
+};
+
+searchForUser = function (email) {
+    var user = serverstub.getUserDataByEmail(localStorage.getItem("token"), email);
+    if (user.success) {
+        document.getElementById("errormessage").innerHTML = "";
+        displayHome(email);
+    }
+    else {
+        document.getElementById("successmessage").innerHTML = "";
+        document.getElementById("errormessage").innerHTML = user.message;
+    }
+};
+
+loadWall = function (email) {
+    document.getElementById("textarea").value = "";
+    var messages;
+    if (email == null) {
+        messages = serverstub.getUserMessagesByToken(localStorage.getItem("token"));
+    }
+    else {
+        messages = serverstub.getUserMessagesByEmail(localStorage.getItem("token"), email);
+    }
+    var textarea = document.getElementById("textarea");
+    for (var i = 0; i < messages.data.length; i++) {
+        textarea.value += messages.data[i].writer + ": " + messages.data[i].content + "\n";
+    }
+};
+
+displayHomeScreenInBrowse = function (homeEmail) {
+    var user = serverstub.getUserDataByEmail(localStorage.getItem("token"), homeEmail);
+    document.getElementById("fname").innerHTML = user.data.firstname;
+    document.getElementById("lname").innerHTML = user.data.familyname;
+    document.getElementById("gender").innerHTML = user.data.gender;
+    document.getElementById("city").innerHTML = user.data.city;
+    document.getElementById("country").innerHTML = user.data.country;
+    document.getElementById("email").innerHTML = user.data.email;
+
+    var userHomeScreen = document.getElementById("home").innerHTML;
+
+    document.getElementById("homedisplay").innerHTML = userHomeScreen;
+};
+
+fillInUserInfo = function (email) {
+    var user;
+    if (email == null) {
+        user = serverstub.getUserDataByToken(localStorage.getItem("token"));
+    }
+    else {
+        user = serverstub.getUserDataByEmail(localStorage.getItem("token"), email);
+    }
+    document.getElementById("fname").innerHTML = user.data.firstname;
+    document.getElementById("lname").innerHTML = user.data.familyname;
+    document.getElementById("gender").innerHTML = user.data.gender;
+    document.getElementById("city").innerHTML = user.data.city;
+    document.getElementById("country").innerHTML = user.data.country;
+    document.getElementById("email").innerHTML = user.data.email;
+};
+
+changePassword = function (form) {
+    if (validatePassword(form)) {
+		var token = localStorage.getItem("token");	
+		var s = serverstub.changePassword(token, form.opword.value, form.pword.value)
+		if (s.success) {	
+			form.reset();
+			document.getElementById("errormessage").innerHTML = "";
+			document.getElementById("successmessage").innerHTML = s.message;
+		}
+		else {
+			form.reset();
+			document.getElementById("successmessage").innerHTML = "";
+			document.getElementById("errormessage").innerHTML = s.message;
+		}
+    }
+};
+
+submitLoginForm = function () {
+	var form = document.getElementById("login");
+	if (validatePasswordLength(form)) {
+		var s = serverstub.signIn(form.email.value, form.pword.value);
+		if (s.success) {
+			var token = s.data;
+			localStorage.setItem("token", token);
+			displayView();
+		}
+        else {
+            document.getElementById("errormessage").innerHTML = s.message;
+        }
+	}
+};
+
+submitSignUpForm = function () {
+	var form = document.getElementById("signup");
+	if (validatePassword(form)) {
+		var g = document.getElementById("gender");
+		var f = {
+			email:form.email.value,
+			password:form.pword.value,
+			firstname:form.fname.value,
+			familyname:form.lname.value,
+			gender:g.options[g.selectedIndex].value,
+			city:form.city.value,
+			country:form.country.value
+		};
+		
+		var s = serverstub.signUp(f);
+		if (s.success) {
+			document.getElementById("successmessage").innerHTML = s.message;
+			form.reset();
+		}
+		else {
+			document.getElementById("errormessage").innerHTML = s.message;
+		}
+	}
+};
+
+validatePasswordLength = function (form) {
+	if (form.pword.value.length < 6) {
+		document.getElementById("errormessage").innerHTML = "Password must be at least 6 characters long!";
+		return false;
+    }
+	else {
+		document.getElementById("errormessage").innerHTML = "";
+		return true;
+	}
+};
+
+validatePassword = function (form) {
+    if (form.pword.value != form.rpword.value) {
+		document.getElementById("successmessage").innerHTML = "";
+		document.getElementById("errormessage").innerHTML = "Passwords does not match!";
+		return false;
+    }
+	
+	return validatePasswordLength(form);
+};
+
+window.onload = function () {
+    displayView();
+};
