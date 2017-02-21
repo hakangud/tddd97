1237b365e74929b0820924694af528b53e2fb383
Håkan Gudmundsson <hakgu806@student.liu.se>
2017-02-21 11:39:01 +0100

    update database_helper.py & server.py

--- twidder/server.py
+++ twidder/server.py
@@ -3,21 +3,16 @@ from random import randint
 from flask import request, Flask
 import json
 
-app = Flask(__name__, static_url_path='/static')
-app.config.from_object(__name__)
+app = Flask(__name__)
 
 logged_in_users = {}
-
-@app.route('/')
-def index():
-    return app.send_static_file('client.html')
 
 @app.route('/signin', methods=['POST'])
 def sign_in():
     email = request.form['email']
     password = request.form['password']
     if dh.validate_password(email, password):
-        letters = "abcdefghiklmnopqrstuvwwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+        letters = "abcdefghiklmnopqrstuvwwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         token = ""
         for i in range(36):
           token += letters[randint(0, len(letters)-1)]
@@ -36,8 +31,8 @@ def sign_up():
     gender = request.form['gender']
     city = request.form['city']
     country = request.form['country']
-    if dh.validate_user(email) == False:
-        dh.sign_up(email, password, firstname, familyname, gender, city, country)
+    if not dh.validate_user(email):
+        dh.add_user(email, password, firstname, familyname, gender, city, country)
         return json.dumps({"success": True, "message": "Successfully created a new user"}) 
 
     return json.dumps({"success": False, "message": "User already exists"})
@@ -47,7 +42,6 @@ def sign_out():
     token = request.form['token']
     if token in logged_in_users:
         del logged_in_users[token]
-        sign_out(token)
         return json.dumps({"success": True, "message": "Successfully signed out"})
     
     return json.dumps({"success": False, "message": "You are not signed in"})
@@ -67,20 +61,17 @@ def change_password():
 
     return json.dumps({"success": False, "message": "You are not signed in"})
 
-@app.route('/getuserdatabytoken', methods=['POST'])
-def get_user_data_by_token():
-    token = request.form['token']
+@app.route('/getuserdatabytoken/<token>', methods=['GET'])
+def get_user_data_by_token(token):
     if token in logged_in_users:
         email = logged_in_users[token]
         return get_user_data(email)
 
     return json.dumps({"success": False, "message": "You are not signed in"})
 
-@app.route('/getuserdatabyemail', methods=['POST'])
-def get_user_data_by_email():
-    token = request.form['token']
+@app.route('/getuserdatabyemail/<token>/<email>', methods=['GET'])
+def get_user_data_by_email(token, email):
     if token in logged_in_users:
-        email = request.form['email']
         return get_user_data(email)
 
     return json.dumps({"success": False, "message": "You are not signed in"})
@@ -100,20 +91,17 @@ def get_user_data(email):
 
     return json.dumps({"success": False, "message": "No such user"})
 
-@app.route('/getusermessagesbytoken', methods=['POST'])
-def get_user_messages_by_token():
-    token = request.form['token']
+@app.route('/getusermessagesbytoken/<token>', methods=['GET'])
+def get_user_messages_by_token(token):
     if token in logged_in_users:
         email = logged_in_users[token]
         return get_user_messages(email)
         
     return json.dumps({"success": False, "message": "You are not signed in"})
 
-@app.route('/getusermessagesbyemail', methods=['POST'])
-def get_user_messages_by_email():
-    token = request.form['token']
+@app.route('/getusermessagesbyemail/<token>/<email>', methods=['GET'])
+def get_user_messages_by_email(token, email):
     if token in logged_in_users:
-        email = request.form['email']
         return get_user_messages(email)
 
     return json.dumps({"success": False, "message": "You are not signed in"})
@@ -133,7 +121,7 @@ def post_message():
     if token in logged_in_users:
         sender_email = logged_in_users[token]
         if dh.validate_user(reciever_email):
-            dh.post_message(reciever_email, sender_email, message)
+            dh.add_message(reciever_email, sender_email, message)
             return json.dumps({"success": True, "message": "Message posted"})
         else:
             return json.dumps({"success": False, "message": "No such user"})
